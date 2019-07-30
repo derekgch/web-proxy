488ae3533430e867813614f5b1d59f174a414e9b
Derek G Chen <derek@derekgc.com>
2019-07-30 11:00:25 -0400

    added input fields

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -1,13 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 interface Props {
   
 }
 
 const InputForm: React.FC<Props> = (props) => {
+  const [ url, setUrl ] = useState('');
+  const [ optional, setOptional ] = useState('');
+
+  const handleChange=(event:React.ChangeEvent<HTMLInputElement>)=>{
+    const { name, value } = event.target;
+    console.log(name);
+    console.log(value);
+    switch (name) {
+      case 'url':
+        setUrl(value);
+        break;
+      case 'optional':
+        setOptional(value);
+        break;
+      default:
+        break;
+    }
+  }
   return (
     <div>
-      Form!
+      <label defaultValue={"URL:"}>
+        <input  type='text' 
+                value={url} 
+                placeholder={"Input url"} 
+                onChange={handleChange}
+                name="url">
+        </input> 
+      </label>
+      <label defaultValue={"Optional:"}>
+        <input  type='text' 
+                value={optional} 
+                onChange={handleChange}
+                placeholder={"optional input field"} >
+        </input>
+      </label>
     </div>
   )
 }
